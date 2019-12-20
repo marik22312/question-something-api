0a95d62d351110a5b4dfaa321b70bb9cb2f9f769
mark22312 <mark22312@gmail.com>
2019-12-20 23:12:30 +0200

    finished routes setup

---
+++ src/routes/categories/index.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router
+	.get('/', (req: Request, res: Response) => {
+		res.send('getAllCategories')
+	});
+
+export const CategoriesRoutes = router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { QuestionsRoutes } from './questions';
+import { DifficultiesRoutes } from './difficulties';
+import { CategoriesRoutes } from './categories';
 
 const router = Router();
 
-router.use('/questions', QuestionsRoutes)
+router.use('/questions', QuestionsRoutes).use('/difficulties', DifficultiesRoutes).use('/categories', CategoriesRoutes)
 
 export const questionsSomethingApiRoutes = router;
