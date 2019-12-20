dbaa47575ec68f472fc2d0cf79d159f183f34e24
mark22312 <mark22312@gmail.com>
2019-12-21 01:38:05 +0200

    Referenced questions to categories and difficulties

--- src/schemas/question.schema.ts
+++ src/schemas/question.schema.ts
@@ -4,6 +4,8 @@ export interface IQuestion extends Document {
 	question: string;
 	no_of_likes: number;
 	no_of_dislikes: number;
+	categories: Schema.Types.ObjectId;
+	difficulties: Schema.Types.ObjectId;
 }
 
 export const QuestionSchema: Schema = new Schema({
@@ -20,5 +22,13 @@ export const QuestionSchema: Schema = new Schema({
 	no_of_dislikes: {
 		type: Schema.Types.Number,
 		default: 0,
+	},
+	categories: {
+		type: [Schema.Types.ObjectId],
+		ref: 'category',
+	},
+	difficulties: {
+		type: [Schema.Types.ObjectId],
+		ref: 'difficulty',
 	},
 });
