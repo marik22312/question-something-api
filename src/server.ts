e2d13bb51c8c9c4212d75514c25756bb664db460
mark22312 <mark22312@gmail.com>
2019-12-20 20:42:22 +0200

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import Express, { Express as ExpressInstance, Request, Response } from 'express';
+import BodyParser from 'body-parser';
+
+export class Server {
+	
+	private readonly server: ExpressInstance;
+	private _port: number;
+
+	constructor() {
+		this.server = Express();
+		this._port = 4200;
+	}
+
+	public listen(port?: number): void {
+		this._port = port || this._port;
+
+		this.withRoute();
+
+		this.server.listen(this._port, () => {
+			console.log(`Server is listening on port ${this._port}`);
+		})
+		return;
+	}
+
+	private withRoute() {
+		this.server.get('*', (req: Request, res: Response) => {
+			res.sendStatus(200);
+		});
+	}
+
+	public get port() {
+		return this._port;
+	}
+
+	public kill() {
+		return;
+	}
+}
