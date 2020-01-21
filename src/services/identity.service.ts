9489ce5ef5c91999840e0f45ba841abb0d0437dd
mark22312 <mark22312@gmail.com>
2020-01-22 00:54:25 +0200

    lint

--- src/services/identity.service.ts
+++ src/services/identity.service.ts
@@ -1,7 +1,7 @@
-import { IUsers } from '../schemas';
-import { sign, verify } from 'jsonwebtoken';
-import { UsersService } from '../controllers/UsersService';
-import bcrypt from 'bcrypt';
+import { IUsers } from "../schemas";
+import { sign, verify } from "jsonwebtoken";
+import { UsersService } from "../controllers/UsersService";
+import bcrypt from "bcrypt";
 
 export interface IdentityServiceConstructorProps {
 	secret: string;
@@ -9,11 +9,11 @@ export interface IdentityServiceConstructorProps {
 }
 
 export interface AuthUserObj {
-	_id: IUsers['_id'];
-	email: IUsers['email'];
+	_id: IUsers["_id"];
+	email: IUsers["email"];
 }
 export interface VerifiedUserDto {
-	_id: IUsers['_id'];
+	_id: IUsers["_id"];
 }
 
 export interface AuthenticationResponse {
@@ -22,59 +22,61 @@ export interface AuthenticationResponse {
 }
 
 export class IdentityService {
-					public static readonly DEFAULT_TOKEN_EXPIRATION: number =
-						1000 * 60 * 60 * 15; // ms s m d
+	public static readonly DEFAULT_TOKEN_EXPIRATION: number = 1000 * 60 * 60 * 15; // ms s m d
 
-					private readonly secret: string;
-					private readonly usersService: UsersService;
+	private readonly secret: string;
+	private readonly usersService: UsersService;
 
-					constructor(constructorObj: IdentityServiceConstructorProps) {
-						this.secret = constructorObj.secret;
-						this.usersService = constructorObj.usersService;
-					}
+	constructor(constructorObj: IdentityServiceConstructorProps) {
+		this.secret = constructorObj.secret;
+		this.usersService = constructorObj.usersService;
+	}
 
-					public signAuthToken(user: AuthUserObj): string {
-						return sign(user, this.secret, {
-							expiresIn: IdentityService.DEFAULT_TOKEN_EXPIRATION,
-						});
-					}
+	public signAuthToken(user: AuthUserObj): string {
+		return sign(user, this.secret, {
+			expiresIn: IdentityService.DEFAULT_TOKEN_EXPIRATION,
+		});
+	}
 
-					public verifyToken(token: string): AuthUserObj | null {
-						try {
-							return verify(token, this.secret) as AuthUserObj;
-						} catch (err) {
-							return null;
-						}
-					}
+	public verifyToken(token: string): AuthUserObj | null {
+		try {
+			return verify(token, this.secret) as AuthUserObj;
+		} catch (err) {
+			return null;
+		}
+	}
 
-					public async authenticateUserByEmailAndPassword(email: string, password: string): Promise<AuthenticationResponse> {
-						const user = await this.usersService.getUserByEmail(email);
-						if (!user) {
-							throw Error("Wrong email or password");
-						}
-						const isMatch = await this.compareHash(password, user.password);
-						if (!isMatch ) {
-							throw Error("Wrong email or password");
-						}
+	public async authenticateUserByEmailAndPassword(
+		email: string,
+		password: string,
+	): Promise<AuthenticationResponse> {
+		const user = await this.usersService.getUserByEmail(email);
+		if (!user) {
+			throw Error("Wrong email or password");
+		}
+		const isMatch = await this.compareHash(password, user.password);
+		if (!isMatch) {
+			throw Error("Wrong email or password");
+		}
 
-						const userAuthObj: AuthUserObj = {
-							_id: user._id,
-							email: user.email,
-						};
-						const token = this.signAuthToken(userAuthObj);
-						const response: AuthenticationResponse = {
-							user: userAuthObj,
-							token,
-						};
+		const userAuthObj: AuthUserObj = {
+			_id: user._id,
+			email: user.email,
+		};
+		const token = this.signAuthToken(userAuthObj);
+		const response: AuthenticationResponse = {
+			user: userAuthObj,
+			token,
+		};
 
-						return response;
-					}
+		return response;
+	}
 
-					private hashPassword(password: string): Promise<string> {
-						return bcrypt.hash(password, 10);
-					}
+	private hashPassword(password: string): Promise<string> {
+		return bcrypt.hash(password, 10);
+	}
 
-					private compareHash(password: string, hash: string): Promise<boolean> {
-						return bcrypt.compare(password, hash);
-					}
-				}
+	private compareHash(password: string, hash: string): Promise<boolean> {
+		return bcrypt.compare(password, hash);
+	}
+}
